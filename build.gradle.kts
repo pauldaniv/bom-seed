228ee5e4e56c393d6340e70da54ef3a043d8e20e
Paul <paulsegfault@gmail.com>
2020-05-17 06:15:46 +0300

    Added kotlin dsl support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import groovy.util.Node
 import java.io.StringReader
 import java.util.*
 
@@ -8,65 +9,60 @@ plugins {
 group = "com.paul"
 version = getTagOrDefault("1.0.0-SNAPSHOT")
 
+val deployUsr: String = (project.findProperty("gpr.usr") ?: System.getenv("USERNAME") ?: "").toString()
+val deployKey: String = (project.findProperty("gpr.key") ?: System.getenv("TOKEN") ?: System.getenv("GITHUB_TOKEN")).toString()
+val repoKey: String = System.getenv("GITHUB_REPOSITORY") ?: "$deployUsr/${projectDir.name}"
+val publishUrl: String = "https://maven.pkg.github.com/$repoKey"
 
 publishing {
     repositories {
-        maven {
-            name = "GitHub"
-            url = uri("https://maven.pkg.github.com/" + System.getenv("GITHUB_REPOSITORY"))
-            credentials {
-                username = ""
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
+        mavenLocal()
+//        maven {
+//            name = "GitHub"
+//            url = uri(publishUrl)
+//            credentials {
+//                username = deployUsr
+//                password = deployKey
+//            }
+//        }
     }
     publications {
-        create<MavenPublication>("mavenJava") {
-
+        create<MavenPublication>("maven") {
             pom.withXml {
+                val project = asNode()
+                val srcDir = "$projectDir/versions"
 
-                val versionPropertiesFile = File("$projectDir/src/main/versions/_versions.properties")
                 val versionProperties = Properties()
-                versionProperties.load(StringReader(versionPropertiesFile.toString()))
-                properties.set(versionProperties.toMutableMap())
-                println(properties)
+                versionProperties.load(StringReader(file("$srcDir/versions.properties").readText()))
 
-//                xml.children().last() + {
-//                    var mkp = delegate
-//                    mkp.dependencyManagement {
-//                        mkp.dependencies {
-//                            for (propertyFile: File in File("$projectDir/src/main/versions").listFiles()) {
-//
-//                                if (propertyFile.name == '_versions.properties') {
-//                                    continue
-//                                }
-//
-//                                val groupIdFromFile: String = propertyFile.name - '.properties'
-//
-//                                val versions: = Properties()
-//                                versions.load(StringReader(propertyFile.toString()))
-//
-//                                for (pair in versions.entrySet()) {
-//                                    mkp.dependency {
-//                                        val finalVersion = pair . value ==~ /\d.*/ ? pair.value : "\${$pair.value}"
-//                                        mkp.groupId groupIdFromFile
-//                                                mkp.artifactId pair . key
-//                                                mkp.version finalVersion
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
+                val propertiesNode = Node(project, "properties")
+                versionProperties.entries.forEach { propertiesNode.appendNode(it.key, it.value) }
+
+                val dependencyManagement = Node(project, "dependencyManagement")
+                val dependencies = Node(dependencyManagement, "dependencies")
+
+                file("$srcDir/dependencies").listFiles()?.forEach {
+                    val groupIdFromFile = it.name.removeSuffix(".prooperties")
+                    val dependencyVersions = Properties()
+                    dependencyVersions.load(StringReader(it.readText()))
+                    dependencyVersions.entries.forEach { v ->
+                        val dependency = Node(dependencies, "dependency")
+                        dependency.appendNode("groupId", groupIdFromFile)
+                        dependency.appendNode("artifactId", v.key)
+                        dependency.appendNode("version",
+                                if (versionProperties.containsKey(v.value)) v.value else "\${$v.value}")
+                    }
+                }
             }
         }
     }
 }
 
+
 fun getTagOrDefault(defaultValue: String): String {
     val ref: String = System.getenv("GITHUB_REF") ?: return defaultValue
-    if (ref.startsWith("refs/tags/")) {
-        return ref.substring("refs/tags/".length)
-    }
-    return defaultValue
+    return if (ref.startsWith("refs/tags/"))
+        ref.substring("refs/tags/".length)
+    else
+        defaultValue
 }
